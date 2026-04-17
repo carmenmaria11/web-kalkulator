e18fc537819e23885529533c0ac33d7cb1e8338a
carmenmaria11 <carmen.maria.kleijnen@gmail.com>
2026-04-17 09:41:35 +0200

    Test bug

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 export function fizzBuzz(tall) {
-    if (tall % 15 === 0) return "FizzBuzz";
+    if (tall % 14 === 0) return "FizzBuzz";
     if (tall % 3 === 0) return "Fizz";
     if (tall % 5 === 0) return "Buzz";
     return tall.toString();
